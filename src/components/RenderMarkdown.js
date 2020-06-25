bb8a6b545a1837a54b3c053bc59b6d0222436f24
songyu <songyu98@gmail.com>
2020-06-25 17:18:55 +0800

    component cleanup for rendermarkdown

--- src/components/RenderMarkdown.js
+++ src/components/RenderMarkdown.js
@@ -3,8 +3,7 @@ import ReactMarkdown from 'react-markdown/with-html'
 import { useLocation } from 'react-router-dom'
 import { Container, Jumbotron } from 'react-bootstrap'
 
-function RenderMarkdown(props) {
-    const { file, onRender, components} = props
+function RenderMarkdown({ file, onRender, components}) {
     const [body, setBody] = useState('')
     const baseurl = 'http://doc.vouch.sg'
     let fullurl = baseurl + file + '/index.html.md'
@@ -12,18 +11,27 @@ function RenderMarkdown(props) {
         fullurl = 'http://doc.vouch.sg/index.html.md'
     }
     const location = useLocation().pathname
-    onRender(location, file)
     useEffect(() => {
+        onRender(location, file)
+        const abortController = new AbortController()
         const fetchData = async () => {
-            const result = await fetch(fullurl, {mode: 'cors', method: 'get'})
-            if (result.status === 404) {
-                setBody('# Page has yet to be uploaded, click on Services - New Infra for uploaded document')
-            } else {
-                const text = await result.text()
-                setBody(text)
+            try {
+                const result = await fetch(fullurl, {mode: 'cors', method: 'get', signal: abortController.signal})
+                if (result.status === 404) {
+                    setBody('# Page has yet to be uploaded, click on Services - New Infra for uploaded document')
+                } else {
+                    const text = await result.text()
+                    setBody(text)
+                }
+            } catch (err) {
+                console.log(err)
             }
         }
         fetchData()
+
+        return () => {
+            abortController.abort()
+        }
     })
     return (
         <>
